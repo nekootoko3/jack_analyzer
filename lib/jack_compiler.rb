0c629f605b1144c17609545700352987f2c54f5d
nekootoko3 <miyako.kerria@gmail.com>
2020-03-29 13:31:35 +0900

    bundle gem jack_compiler

---
+++ lib/jack_compiler.rb
@@ -0,0 +1,6 @@
+require "jack_compiler/version"
+
+module JackCompiler
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/jack_compiler_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe JackCompiler do
+  it "has a version number" do
+    expect(JackCompiler::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
