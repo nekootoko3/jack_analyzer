74766632d54deeae3dca88d142607f498b8f81a2
nekootoko3 <miyako.kerria@gmail.com>
2020-03-30 13:57:29 +0900

    Implemented tokenizer

--- lib/jack_compiler/cli.rb
+++ lib/jack_compiler/cli.rb
@@ -1,5 +1,33 @@
+require "jack_compiler/jack_tokenizer"
+
 module JackCompiler::Cli
   def self.start(args)
-    puts args
+    input_files = case File.ftype(args[0]).to_sym
+      when :file
+        [args[0]]
+      when :directory
+        Dir.glob("#{args[0]}/*.jack")
+      else
+        raise "Invalid file type #{File.ftype(args[0])}"
+      end
+
+    input_files.each do |input_file|
+      puts "processing #{input_file}..."
+
+      output_file = File.open(
+        File.join(File.dirname(input_file), File.basename(input_file, ".*") + ".xml"),
+        "w+"
+      )
+      tokenizer = JackCompiler::JackTokenizer.new(input_file)
+
+      output_file.puts("<tokens>")
+      while tokenizer.has_more_tokens?
+        tokenizer.advance!
+        output_file.puts("<#{tokenizer.token_type}> #{tokenizer.token} </#{tokenizer.token_type}>")
+      end
+      output_file.puts("</tokens>")
+
+      puts "processed #{input_file}"
+    end
   end
 end
---
+++ lib/jack_compiler/jack_tokenizer.rb
@@ -0,0 +1,119 @@
+require "jack_compiler/token_type"
+
+class JackCompiler::JackTokenizer
+  include JackCompiler::TokenType
+
+  def initialize(input_file)
+    @input = File.open(input_file).read
+    @location = 0
+    @token = nil
+    @token_type = nil
+  end
+
+  def token_type
+    str_from(@token_type)
+  end
+
+  def token
+    tokenize_from(@token)
+  end
+
+  # @return [Boolean]
+  def has_more_tokens?
+    while true
+      case
+      when @input[@location]&.match?(/\s/)
+        skip_whitespace_and_newline!
+      when @input[@location, 2] == "//"
+        skip_single_line_comment!
+      when @input[@location, 2] == "/*"
+        skip_multiple_line_comment!
+      else
+        break
+      end
+
+      break if @input[@location].nil?
+    end
+
+
+    !@input[@location].nil?
+  end
+
+  def advance!
+    set_token!
+    @location += 1
+  end
+
+  def keyword
+
+  end
+
+  def symbol
+
+  end
+
+  def identifier
+
+  end
+
+  def inv_val
+
+  end
+
+  def string_val
+
+  end
+
+  private
+
+  def skip_single_line_comment!
+    while !@input[@location].match?(/\n/)
+      @location += 1
+    end
+
+    @location += 1
+  end
+
+  def skip_multiple_line_comment!
+    while @input[@location, 2] != "*/"
+      @location += 1
+    end
+    @location += 2
+  end
+
+  def skip_whitespace_and_newline!
+    while @input[@location]&.match?(/\s/)
+      @location += 1
+    end
+  end
+
+  def set_token!
+    start = @location
+
+    case
+    when is_symbol?(@input[@location])
+      @token_type = SYMBOL
+    when is_integer_constant?(@input[@location])
+      while @input[@location+1].match?(/\d/)
+        @location += 1
+      end
+      @token_type = INT_CONST
+    when is_string_constant?(@input[@location])
+      start += 1 # in case of string constant, start is ".
+      @location += 1
+      while !@input[@location+1].match?(/"/)
+        @location += 1
+      end
+      @token_type = STRING_CONST
+    else
+      while @input[@location+1].match?(/\w/)
+        @location += 1
+      end
+      @token_type = is_keyword?(@input[start..@location]) ?
+        KEYWORD : IDENTIFIER
+    end
+
+    @token = @input[start..@location]
+    @location += 1 if @token_type == STRING_CONST # in case of string constant, location is ".
+  end
+end
